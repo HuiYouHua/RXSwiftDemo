1f5e67942a09480780ed754c1d13eea09524b520
华惠友 <huiyou.hua@2339.com>
2020-05-08 19:47:55 +0800

    -a

--- 17UI控件扩展1：UILabel/ViewController.swift
+++ 17UI控件扩展1：UILabel/ViewController.swift
@@ -11,21 +11,49 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-
+    
     let disposeBag = DisposeBag()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //创建文本标签
+        let label = UILabel(frame:CGRect(x:20, y:40, width:300, height:100))
+        self.view.addSubview(label)
+        
+        //创建一个计时器（每0.1秒发送一个索引数）
+        let timer = Observable<Int>.interval(RxTimeInterval.milliseconds(100), scheduler: MainScheduler.instance)
+//        let timer = Observable<Int>.interval(0.1, scheduler: MainScheduler.instance)
+        //将已过去的时间格式化成想要的字符串，并绑定到label上
+        timer.asObservable().map(formatTimeInterval).bind(to: label.rx.attributedText).disposed(by: disposeBag)
+        
+    }
+    
+    //将数字转成对应的富文本
+    func formatTimeInterval(ms: NSInteger) -> NSMutableAttributedString {
+        let string = String(format: "%0.2d:%0.2d.%0.1d",
+                            arguments: [(ms / 600) % 600, (ms % 600 ) / 10, ms % 10])
+        //富文本设置
+        let attributeString = NSMutableAttributedString(string: string)
+        //从文本0开始6个字符字体HelveticaNeue-Bold,16号
+        attributeString.addAttribute(NSAttributedString.Key.font,
+                                     value: UIFont(name: "HelveticaNeue-Bold", size: 16)!,
+                                     range: NSMakeRange(0, 5))
+        //设置字体颜色
+        attributeString.addAttribute(NSAttributedString.Key.foregroundColor,
+                                     value: UIColor.white, range: NSMakeRange(0, 5))
+        //设置文字背景颜色
+        attributeString.addAttribute(NSAttributedString.Key.backgroundColor,
+                                     value: UIColor.orange, range: NSMakeRange(0, 5))
+        return attributeString
     }
 }
 
 // MARK: -
-extension ViewController {
-    //
-    func () {
-        
-    }
-}
+//extension ViewController {
+//    //
+//    func () {
+//
+//    }
+//}
 
 
--- 18UI控件扩展2：UITextField、UITextView/ViewController.swift
+++ 18UI控件扩展2：UITextField、UITextView/ViewController.swift
@@ -17,13 +17,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //创建文本输入框
+            let inputField = UITextField(frame: CGRect(x:10, y:80, width:200, height:30))
+        inputField.borderStyle = .roundedRect
+            self.view.addSubview(inputField)
+             
+            //创建文本输出框
+            let outputField = UITextField(frame: CGRect(x:10, y:150, width:200, height:30))
+            outputField.borderStyle = .roundedRect
+            self.view.addSubview(outputField)
+             
+            //创建文本标签
+            let label = UILabel(frame:CGRect(x:20, y:190, width:300, height:30))
+            self.view.addSubview(label)
+             
+            //创建按钮
+            let button:UIButton = UIButton(type:.system)
+            button.frame = CGRect(x:20, y:230, width:40, height:30)
+            button.setTitle("提交", for:.normal)
+            self.view.addSubview(button)
+             
+             
+            //当文本框内容改变
+            let input = inputField.rx.text.orEmpty.asDriver() // 将普通序列转换为 Driver
+                .throttle(0.3) //在主线程中操作，0.3秒内值若多次改变，取最后一次
+             
+            //内容绑定到另一个输入框中
+            input.drive(outputField.rx.text)
+                .disposed(by: disposeBag)
+             
+            //内容绑定到文本标签中
+            input.map{ "当前字数：\($0.count)" }
+                .drive(label.rx.text)
+                .disposed(by: disposeBag)
+             
+            //根据内容字数决定按钮是否可用
+            input.map{ $0.count > 5 }
+                .drive(button.rx.isEnabled)
+                .disposed(by: disposeBag)
+        
+        inputField.rx.controlEvent([.editingDidBegin]) //状态可以组合
+        .asObservable()
+        .subscribe(onNext: { _ in
+            print("开始编辑内容!")
+        }).disposed(by: disposeBag)
+       
     }
 }
 
 // MARK: -
-extension ViewController {
-    //
-    func () {
-        
-    }
-}
+//extension ViewController {
+//    //
+//    func () {
+//
+//    }
+//}
